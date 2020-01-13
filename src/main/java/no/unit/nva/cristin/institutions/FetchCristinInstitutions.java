94d482e29f802e5feab71548ec16992defec36ef
nr <nr@unit.no>
2020-01-13 14:41:15 +0100

    removed unused imports

--- src/main/java/no/unit/nva/cristin/institutions/FetchCristinInstitutions.java
+++ src/main/java/no/unit/nva/cristin/institutions/FetchCristinInstitutions.java
@@ -14,8 +14,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
